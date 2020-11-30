cf61bbf6c2ffa3803e33e4f4b1c5d74bf1db2f5f
Hristo Dzhambov <dzhambov@hotmail.com>
2020-11-30 17:26:11 -0500

    add state.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
-import Navbar from './components/layouts/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <UserItem />
       </div>
     );
   }

60264584f9cce043fb5605cbc801a2b771152d57
Hristo Dzhambov <dzhambov@hotmail.com>
2020-12-04 15:33:47 -0500

    add clear users from state.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,6 +9,8 @@ export class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onChange = (e) => {
@@ -22,6 +24,8 @@ export class Search extends Component {
   }
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form 
@@ -36,8 +40,9 @@ export class Search extends Component {
           />
           <input type="submit" value="Search" className="btn btn-dark btn-block"/>
         </form>
+        {showClear && (<button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>)}   
       </div>
-    )
+    );
   }
 }
 

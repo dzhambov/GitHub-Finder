906b21651404013a2d8f4bfd919fbf029468789d
Hristo Dzhambov <dzhambov@hotmail.com>
2020-12-21 16:06:53 -0500

    add react router.

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import About from '../pages/About';
 
 const Navbar = ({icon, title}) => {
   
@@ -8,9 +10,17 @@ const Navbar = ({icon, title}) => {
       <h1>
         <i className={icon} /> {title}
         </h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
     </nav>
-  )
-}
+  );
+};
 
 Navbar.defaultProps = {
   title: 'Github Finder',
